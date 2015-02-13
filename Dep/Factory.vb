0efbf6a0c30d0a858c8dcd1dd5dcc74b83ad7a90
plmeister@gmail.com <plmeister@gmail.com>
2015-02-13 00:37:32 +0000

    add factory class with static functions to create generic instances of observable and computed with type inference

---
+++ Dep/Factory.vb
@@ -0,0 +1,10 @@
+﻿Public Class Factory
+    Public Shared Function Observable(Of T)(value As T) As Observable(Of T)
+        Return New Observable(Of T)(value)
+    End Function
+
+    Public Shared Function Computed(Of T)(f As Func(Of T)) As Computed(Of T)
+        Return New Computed(Of T)(f)
+    End Function
+
+End Class
--- Dep/Observable.vb
+++ Dep/Observable.vb
@@ -29,5 +29,4 @@
         Return Value.ToString()
     End Function
 
-
 End Class
