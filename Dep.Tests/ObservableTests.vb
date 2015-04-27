11080ed89d2ca85fb727fa9f9539c42f94750b12
plmeister@gmail.com <plmeister@gmail.com>
2015-04-27 20:27:12 +0100

    include new tests for observable

---
+++ Dep.Tests/ObservableTests.vb
@@ -0,0 +1,20 @@
+﻿Imports NUnit.Framework
+
+<TestFixture> Public Class ObservableTests
+    <Test> Public Sub SetDifferentValueRaisesEvent()
+        Dim x = Dep.Factory.Observable("abc")
+        Dim eventRaised As Boolean = False
+        AddHandler x.PropertyChanged, Sub() eventRaised = True
+
+        x.Value = "def"
+        Assert.True(eventRaised)
+    End Sub
+
+    <Test> Public Sub SetSameValueDoesNotRaiseEvent()
+        Dim x = Dep.Factory.Observable("zzz")
+        Dim eventRaised As Boolean = False
+        AddHandler x.PropertyChanged, Sub() eventRaised = True
+        x.Value = "zzz"
+        Assert.False(eventRaised)
+    End Sub
+End Class
